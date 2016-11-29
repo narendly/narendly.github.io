a89a60de664163b74dd7d3a6f5857ed3fc7fa2ad
Hunter Lee <narendly@gmail.com>
2016-11-28 21:24:44 -0500

    try

--- files/script.sh
+++ files/script.sh
@@ -17,8 +17,8 @@ S=20mb #cachesize
 CORE=0x8000 #core
 
 #20mb
-sudo taskset "$CORE" $PARSEDIR/parsecmgmt -a run -p blackscholes -i simsmall >> $OUTDIR/blackscholes/raw/bs_"$S"_raw
-grep -E '^(IPC*|Total*)' $OUTDIR/blackscholes/raw/bs_"$S" >> $OUTDIR/blackscholes/bs_"$S"
+sudo taskset "$CORE" $PARSEDIR/parsecmgmt -a run -p blackscholes -i simsmall >> $OUTDIR/blackscholes/bs_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/blackscholes/bs_"$S"_raw >> $OUTDIR/blackscholes/bs_"$S"
 echo -e "blackscholes   "$S":  " >> $OUTDIR/blackscholes/links
 fpaste $OUTDIR/blackscholes/bs_"$S" >> $OUTDIR/blackscholes/links
 echo -e "\n" >> $OUTDIR/blackscholes/links
@@ -28,8 +28,8 @@ echo -e "\n" >> $OUTDIR/blackscholes/links
 S=1mb #cachesize
 CORE=0x400 #core
 #1mb
-sudo taskset "$CORE" $PARSEDIR/parsecmgmt -a run -p blackscholes -i simsmall >> $OUTDIR/blackscholes/raw/bs_"$S"_raw
-grep -E '^(IPC*|Total*)' $OUTDIR/blackscholes/raw/bs_"$S" >> $OUTDIR/blackscholes/bs_"$S"
+sudo taskset "$CORE" $PARSEDIR/parsecmgmt -a run -p blackscholes -i simsmall >> $OUTDIR/blackscholes/bs_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/blackscholes/bs_"$S"_raw >> $OUTDIR/blackscholes/bs_"$S"
 echo -e "blackscholes   "$S":  " >> $OUTDIR/blackscholes/links
 fpaste $OUTDIR/blackscholes/bs_"$S" >> $OUTDIR/blackscholes/links
 echo -e "\n" >> $OUTDIR/blackscholes/links

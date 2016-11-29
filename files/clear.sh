1d114eefb8501a899a73a82f636715a20bb59c1b
Hunter Lee <narendly@gmail.com>
2016-11-28 23:24:52 -0500

    h

---
+++ files/clear.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUTDIR=/home/hl130/parsec-3.0/output
+
+sudo rm $OUTDIR/links
+sudo rm $OUTDIR/blackscholes/*
+sudo rm $OUTDIR/canneal/*
+sudo rm $OUTDIR/dedup/*
+sudo rm $OUTDIR/ferret/*
+sudo rm $OUTDIR/swaptions/*
+

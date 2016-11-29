ece80a0b4bc754ee41bde0087e98bb20b49c5ae8
Hunter Lee <narendly@gmail.com>
2016-11-28 22:21:14 -0500

    ASDf

---
+++ files/func.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+PARSECDIR=/home/hl130/parsec-3.0/bin
+OUTDIR=/home/hl130/parsec-3.0/output
+
+
+
+#order
+#1) run it and save output
+#2) grep it
+#3) fpaste it and save the link
+
+ACTION=run
+DATASET=native
+SLEEP=10s
+
+#APP
+
+function run_experiment {
+
+APP=$1
+DATASET=$2
+
+S=20mb #cachesize
+CORE=0x8000 #core
+
+sudo taskset "$CORE" /home/hl130/parsec-3.0/bin/parsecmgmt -a "$ACTION" -p "$APP" -i "$DATASET" >> $OUTDIR/"$APP"/"$APP"_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/"$APP"/"$APP"_"$S"_raw >> $OUTDIR/"$APP"/"$APP"_"$S"
+echo -e ""$APP"   "$S":  " >> $OUTDIR/links
+sudo fpaste $OUTDIR/"$APP"/"$APP"_"$S" >> $OUTDIR/links
+echo -e "\n" >> $OUTDIR/links
+
+
+sleep $SLEEP
+
+S=19mb #cachesize
+CORE=0x4000 #core
+
+sudo taskset "$CORE" /home/hl130/parsec-3.0/bin/parsecmgmt -a "$ACTION" -p "$APP" -i "$DATASET" >> $OUTDIR/"$APP"/"$APP"_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/"$APP"/"$APP"_"$S"_raw >> $OUTDIR/"$APP"/"$APP"_"$S"
+echo -e ""$APP"   "$S":  " >> $OUTDIR/links
+sudo fpaste $OUTDIR/"$APP"/"$APP"_"$S" >> $OUTDIR/links
+echo -e "\n" >> $OUTDIR/links
+
+sleep $SLEEP
+
+S=15mb #cachesize
+CORE=0x2000 #core
+
+sudo taskset "$CORE" /home/hl130/parsec-3.0/bin/parsecmgmt -a "$ACTION" -p "$APP" -i "$DATASET" >> $OUTDIR/"$APP"/"$APP"_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/"$APP"/"$APP"_"$S"_raw >> $OUTDIR/"$APP"/"$APP"_"$S"
+echo -e ""$APP"   "$S":  " >> $OUTDIR/links
+sudo fpaste $OUTDIR/"$APP"/"$APP"_"$S" >> $OUTDIR/links
+echo -e "\n" >> $OUTDIR/links
+
+sleep $SLEEP
+
+S=10mb #cachesize
+CORE=0x1000 #core
+
+sudo taskset "$CORE" /home/hl130/parsec-3.0/bin/parsecmgmt -a "$ACTION" -p "$APP" -i "$DATASET" >> $OUTDIR/"$APP"/"$APP"_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/"$APP"/"$APP"_"$S"_raw >> $OUTDIR/"$APP"/"$APP"_"$S"
+echo -e ""$APP"   "$S":  " >> $OUTDIR/links
+sudo fpaste $OUTDIR/"$APP"/"$APP"_"$S" >> $OUTDIR/links
+echo -e "\n" >> $OUTDIR/links
+
+sleep $SLEEP
+
+S=5mb #cachesize
+CORE=0x800 #core
+
+sudo taskset "$CORE" /home/hl130/parsec-3.0/bin/parsecmgmt -a "$ACTION" -p "$APP" -i "$DATASET" >> $OUTDIR/"$APP"/"$APP"_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/"$APP"/"$APP"_"$S"_raw >> $OUTDIR/"$APP"/"$APP"_"$S"
+echo -e ""$APP"   "$S":  " >> $OUTDIR/links
+sudo fpaste $OUTDIR/"$APP"/"$APP"_"$S" >> $OUTDIR/links
+echo -e "\n" >> $OUTDIR/links
+
+sleep $SLEEP
+
+S=1mb #cachesize
+CORE=0x400 #core
+
+sudo taskset "$CORE" /home/hl130/parsec-3.0/bin/parsecmgmt -a "$ACTION" -p "$APP" -i "$DATASET" >> $OUTDIR/"$APP"/"$APP"_"$S"_raw
+grep -E '^(IPC*|Total*)' $OUTDIR/"$APP"/"$APP"_"$S"_raw >> $OUTDIR/"$APP"/"$APP"_"$S"
+echo -e ""$APP"   "$S":  " >> $OUTDIR/links
+sudo fpaste $OUTDIR/"$APP"/"$APP"_"$S" >> $OUTDIR/links
+echo -e "\n" >> $OUTDIR/links
+
+sleep $SLEEP
+} 
+
+
+
+run_experiment blackscholes simsmall
